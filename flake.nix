3e4295bb77c16a6038b2a865223994dd00303dcb
klchen0112 <klchen0112@gmail.com>
2025-05-04 13:52:51 +0800

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # This pins requirements.txt provided by zephyr-nix.pythonEnv.
-    zephyr.url = "github:zmkfirmware/zephyr/2ab1279768cc852172074994f73227dbfc58ca54";
+    zephyr.url = "github:zmkfirmware/zephyr/v3.5.0+zmk-fixes";
     zephyr.flake = false;
 
     # Zephyr sdk and toolchain.
@@ -15,13 +15,13 @@
   outputs = { nixpkgs, zephyr-nix, ... }: let
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
+    in {
     devShells = forAllSystems (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         zephyr = zephyr-nix.packages.${system};
         keymap_drawer = pkgs.python3Packages.callPackage ./nix/keymap-drawer.nix {};
-      in {
+    in {
         default = pkgs.mkShellNoCC {
           packages =
             [
@@ -35,6 +35,8 @@
               pkgs.just
               pkgs.yq # Make sure yq resolves to python-yq.
 
+              keymap_drawer
+
               # -- Used by just_recipes and west_commands. Most systems already have them. --
               # pkgs.gawk
               # pkgs.unixtools.column
@@ -43,10 +45,7 @@
               # pkgs.findutils # find, xargs
               # pkgs.gnugrep
               # pkgs.gnused
-            ]
-            # Temporary disable keymap_drawer on aarch64-linux due to
-            # https://github.com/NixOS/nixpkgs/issues/372375.
-            ++ nixpkgs.lib.optionals (system != "aarch64-linux") [keymap_drawer];
+            ];
 
           shellHook = ''
             export ZMK_BUILD_DIR=$(pwd)/.build;
